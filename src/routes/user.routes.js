2471d951f560a5e8748d28b10fdb9d9e165f3913
Duke-Saurabh <ds8123495@example.com>
2024-06-23 09:52:40 +0530

    add login route

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { register } from '../controllers/users.register.js';
+import { login } from '../controllers/users.login.js';
 
 const router = Router();
 
-
+router.post('/login', login);
 router.post('/register', register);
 
 export default router;
